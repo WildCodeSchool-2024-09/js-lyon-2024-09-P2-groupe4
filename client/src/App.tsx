9bfc3f70eede4f3579f1b976bc17f8aa5f0473b1
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2024-11-07 17:16:50 +0100

    mise à jour de App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import "./header.css";
+import FavoritesList from "./components/FavoritesList";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import FavoritesList from "./components/FavoritesList";
 
 function App() {
   return (
