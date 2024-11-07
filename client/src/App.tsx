92fcc6c6fa4d0e82e0c019728323181b416a963a
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2024-11-07 16:35:48 +0100

    fix de la barre de boutons +  mise a jour de App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,13 @@ import "./App.css";
 import "./header.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import FavoritesList from "./components/FavoritesList";
 
 function App() {
   return (
     <>
       <Header />
+      <FavoritesList />
       <Footer />
     </>
   );
