a192df6cfd2a6e705ba97334d73e94eee305c7ae
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2024-11-13 16:20:54 +0100

    commit temporaire 2

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import "./Login.css"
-import "../image/header.png"
+import "./Login.css";
+import "../image/header.png";
 
 function Login() {
   //states
@@ -13,31 +13,35 @@ function Login() {
   return (
     <>
       <div className="div-form">
-      <h1>Login Page</h1>
+        <h1>Login Page</h1>
         <form
           onSubmit={(event) => {
             event.preventDefault();
           }}
           className="form"
         >
-            <h2>USERNAME</h2>
-            <input
-              type="text"
-              className="username"
-              placeholder="Votre nom d'utilisateur"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <h2>Password</h2>
-            <input
-              type="password"
-              className="password"
-              placeholder="Votre mot de passe"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+          <h2>USERNAME</h2>
+          <input
+            type="text"
+            className="username"
+            placeholder="Votre nom d'utilisateur"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <h2>Password</h2>
+          <input
+            type="password"
+            className="password"
+            placeholder="Votre mot de passe"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button className="button-login" type="submit">
-            <img className="login-logo" src="src/assets/images/login.png" alt="" />
+            <img
+              className="login-logo"
+              src="src/assets/images/login.png"
+              alt=""
+            />
           </button>
         </form>
       </div>
